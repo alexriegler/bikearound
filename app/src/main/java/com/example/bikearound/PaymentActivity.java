eb5ca60e001393584274639eef587a76083d4140
mathias kniesel <yuriyadaskevych35@gmail.com>
2019-10-20 04:11:00 -0700

    Fix API

--- app/src/main/java/com/example/bikearound/PaymentActivity.java
+++ app/src/main/java/com/example/bikearound/PaymentActivity.java
@@ -1,11 +1,14 @@
 package com.example.bikearound;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.Application;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.text.format.DateFormat;
@@ -28,6 +31,8 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class PaymentActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+    private static final int LINK_REQUEST_CODE = 1;
+    private TextView contentTextView;
 
     private TextView dateText, timeText;
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, MMM d, yyyy");
@@ -60,9 +65,64 @@ public class PaymentActivity extends AppCompatActivity implements DatePickerDial
     }
 
     public void payButton(View v) {
+        Plaid.create(this.getApplication());
         ArrayList<PlaidProduct> products = new ArrayList<>();
         products.add(PlaidProduct.TRANSACTIONS);
-        
+        Plaid.openLink(
+                PaymentActivity.this,
+                new LinkConfiguration.Builder("Test App", products).build(),
+                LINK_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LINK_REQUEST_CODE && data != null) {
+            if (resultCode == Plaid.RESULT_SUCCESS) {
+                LinkConnection item = (LinkConnection) data.getSerializableExtra(Plaid.LINK_RESULT);
+                if (item != null) {
+                    LinkConnectionMetadata metadata = item.getLinkConnectionMetadata();
+                    contentTextView.setText(getString(
+                            R.string.content_success,
+                            item.getPublicToken(),
+                            metadata.getAccounts().get(0).getAccountId(),
+                            metadata.getAccounts().get(0).getAccountName(),
+                            metadata.getInstitutionId(),
+                            metadata.getInstitutionName()));
+                }
+            } else if (resultCode == Plaid.RESULT_CANCELLED) {
+                LinkCancellation cancellation = (LinkCancellation) data.getSerializableExtra(Plaid.LINK_RESULT);
+                if (cancellation != null) {
+                    contentTextView.setText(getString(
+                            R.string.content_cancelled,
+                            cancellation.getInstitutionId(),
+                            cancellation.getInstitutionName(),
+                            cancellation.getLinkSessionId(),
+                            cancellation.getStatus()));
+                }
+            } else if (resultCode == Plaid.RESULT_EXIT) {
+                PlaidApiError error = (PlaidApiError) data.getSerializableExtra(Plaid.LINK_RESULT);
+                if (error != null) {
+                    contentTextView.setText(getString(
+                            R.string.content_exit,
+                            error.getDisplayMessage(),
+                            error.getErrorCode(),
+                            error.getErrorMessage(),
+                            error.getLinkExitMetadata().getInstitutionId(),
+                            error.getLinkExitMetadata().getInstitutionName(),
+                            error.getLinkExitMetadata().getStatus()));
+                }
+            } else if (resultCode == Plaid.RESULT_EXCEPTION) {
+                Exception exception = (Exception) data.getSerializableExtra(Plaid.LINK_RESULT);
+                if (exception != null) {
+                    contentTextView.setText(getString(
+                            R.string.content_exception,
+                            exception.getClass().toString(),
+                            exception.getMessage()));
+                }
+            }
+        }
     }
 
     private void showDatePickerDialog() {
