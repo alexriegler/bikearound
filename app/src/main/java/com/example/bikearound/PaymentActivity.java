afe7af6d3cbdc4abad5bc6bfaadf26e6f5d754b8
Alex Riegler <alexander.s.riegler@gmail.com>
2019-10-20 03:31:13 -0500

    Add date and time pickers

--- app/src/main/java/com/example/bikearound/PaymentActivity.java
+++ app/src/main/java/com/example/bikearound/PaymentActivity.java
@@ -1,15 +1,28 @@
 package com.example.bikearound;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 
-public class PaymentActivity extends AppCompatActivity {
+public class PaymentActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private TextView dateText, timeText;
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, MMM d, yyyy");
+    private SimpleDateFormat simpleTimeFormat = new SimpleDateFormat("h:mm a");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +32,51 @@ public class PaymentActivity extends AppCompatActivity {
         dateText = (TextView) findViewById(R.id.date_text_id);
         timeText = (TextView) findViewById(R.id.time_text_id);
 
+        // Get current time
         long date = System.currentTimeMillis();
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, MMM d, yyyy");
         String dateString = simpleDateFormat.format(date);
         dateText.setText(dateString);
 
-        SimpleDateFormat simpleTimeFormat = new SimpleDateFormat("h:mm a");
         String timeString = simpleTimeFormat.format(date);
         timeText.setText(timeString);
+    }
+
+    public void dateOnClick(View v) {
+        showDatePickerDialog();
+    }
+
+    public void timeOnClick(View v) {
+        showTimePickerDialog();
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        // TODO implement onDateSet()
+    }
+
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                Calendar.getInstance().get(Calendar.MINUTE),
+                false);
+        timePickerDialog.show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // TODO
     }
 }
