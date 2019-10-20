7fc41f94d04797510f8f99bc2b79c7b89eb883b7
Alex Riegler <alexander.s.riegler@gmail.com>
2019-10-20 09:17:59 -0500

    Add costs

--- app/src/main/java/com/example/bikearound/PaymentActivity.java
+++ app/src/main/java/com/example/bikearound/PaymentActivity.java
@@ -14,6 +14,8 @@ import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -26,19 +28,31 @@ import com.plaid.linkbase.models.LinkConnectionMetadata;
 import com.plaid.linkbase.models.PlaidApiError;
 import com.plaid.linkbase.models.PlaidProduct;
 import com.plaid.link.Plaid;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Locale;
 
 public class PaymentActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     private static final int LINK_REQUEST_CODE = 1;
-    private TextView contentTextView;
 
-    private TextView dateText, timeText;
+    private TextView dateText, timeText, rateTypeText, flatFeeText, rateChargeText, totalCostText;
+    private EditText durationEditText;
+
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, MMM d, yyyy");
     private SimpleDateFormat simpleTimeFormat = new SimpleDateFormat("h:mm a");
+
+    private static final int DEFAULT_DURATION = 1;
+    private static final int FLAT_FEE = 5;
+    private static final int HOURLY_RATE = 20;
+    private static final int DAILY_RATE = 45;
+    private static final int WEEKLY_RATE = 100;
+    private int rate, duration, rateCharge, totalCost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +61,19 @@ public class PaymentActivity extends AppCompatActivity implements DatePickerDial
 
         dateText = (TextView) findViewById(R.id.date_text_id);
         timeText = (TextView) findViewById(R.id.time_text_id);
+        rateTypeText = (TextView) findViewById(R.id.rate_type_text_id);
+        flatFeeText = (TextView) findViewById(R.id.flat_fee_text_id);
+        rateChargeText = (TextView) findViewById(R.id.rate_charge_text_id);
+        totalCostText = (TextView) findViewById(R.id.total_text_id) ;
+        durationEditText = (EditText) findViewById(R.id.duration_edit_text_id);
+
+        // Calculate fees
+        rate = HOURLY_RATE;
+        duration = DEFAULT_DURATION;
+        rateCharge = rate * duration;
+        rateChargeText.setText(String.format(Locale.US, "%d.00", rateCharge));
+        totalCost = FLAT_FEE + rateCharge;
+        totalCostText.setText(String.format(Locale.US, "$%d.00", totalCost));
 
         // Get current time
         long date = System.currentTimeMillis();
@@ -123,5 +150,62 @@ public class PaymentActivity extends AppCompatActivity implements DatePickerDial
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         // TODO
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.option1_radio_id:
+                if (checked) {
+                    rateTypeText.setText(R.string.hours);
+                    rate = HOURLY_RATE;
+                    if (!(durationEditText.getText().toString().equals("") & (durationEditText.getText().toString() != null))) {
+                        duration = Integer.parseInt(durationEditText.getText().toString());
+                    }
+                    else {
+                        duration = DEFAULT_DURATION;
+                    }
+                    rateCharge = rate * duration;
+                    rateChargeText.setText(String.format(Locale.US, "%d.00", rateCharge));
+                    totalCost = FLAT_FEE + rateCharge;
+                    totalCostText.setText(String.format(Locale.US, "$%d.00", totalCost));
+                }
+                break;
+            case R.id.option2_radio_id:
+                if (checked) {
+                    rateTypeText.setText(R.string.days);
+                    rate = DAILY_RATE;
+                    if (!(durationEditText.getText().toString().equals("") & (durationEditText.getText().toString() != null))) {
+                        duration = Integer.parseInt(durationEditText.getText().toString());
+                    }
+                    else {
+                        duration = DEFAULT_DURATION;
+                    }
+                    rateCharge = rate * duration;
+                    rateChargeText.setText(String.format(Locale.US, "%d.00", rateCharge));
+                    totalCost = FLAT_FEE + rateCharge;
+                    totalCostText.setText(String.format(Locale.US, "$%d.00", totalCost));
+                }
+                break;
+            case R.id.option3_radio_id:
+                if (checked) {
+                    rateTypeText.setText(R.string.weeks);
+                    rate = WEEKLY_RATE;
+                    if (!(durationEditText.getText().toString().equals("") & (durationEditText.getText().toString() != null))) {
+                        duration = Integer.parseInt(durationEditText.getText().toString());
+                    }
+                    else {
+                        duration = DEFAULT_DURATION;
+                    }
+                    rateCharge = rate * duration;
+                    rateChargeText.setText(String.format(Locale.US, "%d.00", rateCharge));
+                    totalCost = FLAT_FEE + rateCharge;
+                    totalCostText.setText(String.format(Locale.US, "$%d.00", totalCost));
+                }
+                break;
+        }
     }
 }
