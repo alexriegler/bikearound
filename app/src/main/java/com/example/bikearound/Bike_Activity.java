4fe44e58ed87c7a520f634ab8a98f28b768a4df7
Alex Riegler <alexander.s.riegler@gmail.com>
2019-10-20 00:14:42 -0500

    Pass and receive info with and from intent

--- app/src/main/java/com/example/bikearound/Bike_Activity.java
+++ app/src/main/java/com/example/bikearound/Bike_Activity.java
@@ -2,13 +2,35 @@ package com.example.bikearound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Bike_Activity extends AppCompatActivity {
+
+    private TextView tvname, tvtype, tvdescription;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bike_);
+
+        tvname = (TextView) findViewById(R.id.txtName);
+        tvtype = (TextView) findViewById(R.id.txtType);
+        tvdescription = (TextView) findViewById(R.id.txtDesciption);
+        img = (ImageView) findViewById(R.id.bikethumbnail);
+
+        // Receive data
+        Intent intent = getIntent();
+        String name = intent.getExtras().getString("Name");
+        String description = intent.getExtras().getString("Description");
+        int image = intent.getExtras().getInt("Thumbnail");
+
+        // Setting values
+        tvname.setText(name);
+        tvdescription.setText(description);
+        img.setImageResource(image);
     }
 }
